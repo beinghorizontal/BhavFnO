6257455c1fad42e6d6e45702eca1e406d2ed4fab
beinghorizontal <28746824+beinghorizontal@users.noreply.github.com>
2024-02-10 10:25:23 +0530

    for YT_Quantext

--- def_expiry.py
+++ def_expiry.py
@@ -1,13 +1,23 @@
 import calendar
 import pandas as pd
+from make_holidaycsv import get_holidays
 
-def expiry(year, root='path'):
-    df_holidays = pd.read_csv(root + 'indian_holidays.csv')
-    df_holidays['date'] = pd.to_datetime(df_holidays['date'], format='%Y-%m-%d',infer_datetime_format=True).dt.strftime('%d-%m-%Y')
-    "Last Thu of the Month is a holiday. The list is manually created. Checked till 2015, if you want to add the support beyond 2015 then add the dates in 'holiday_list', also if possible share "
-    holiday_list = df_holidays['date'].tolist()
 
+def expiry(year):
+    """
+
+    :param year:
+    :return: It fetch the expiry date in list format for each month
+    for year specified
+
+    """
+
+    df_holidays = get_holidays()
+
+    df_holidays['tradingDate'] = pd.to_datetime(df_holidays['tradingDate'], format='%d-%b-%Y').dt.strftime('%d-%m-%Y')
+    holiday_list = df_holidays['tradingDate'].tolist()
     expiry_list = ['Empty']  # since list starts from 0 this is a hacky way to align expiry_list[1] = first month
+
     for n in range(1, 13):
         month = n
         last_day = calendar.monthrange(year, month)[1]
@@ -21,15 +31,21 @@ def expiry(year, root='path'):
                 break
 
         date_thu = last_day - days
-        "zfill function formats single digits to double digits by adding a leading zero which is needed in NSE option url, comment it if not needed"
+        """
+            zfill function formats single digits to double digits by adding a leading zero which is needed in 
+            NSE option url
+        """
         month_1 = str(month).zfill(2)
         date_1 = str(date_thu).zfill(2)
         expiry = (date_1) + "-" + (month_1) + "-" + str(year)
+
         "check if last Thu of the month (expiry day) is not holiday, if it is holiday then shift the date by one to Wed"
+
         for h in holiday_list:
             if expiry == h:
                 date2 = str(date_thu - 1).zfill(2)
                 expiry = (date2) + "-" + (month_1) + "-" + str(year)
         # print(expiry)
         expiry_list.append(expiry)
-    return (expiry_list)
+
+    return expiry_list
